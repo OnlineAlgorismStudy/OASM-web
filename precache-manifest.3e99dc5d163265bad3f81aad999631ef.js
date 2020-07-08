07056c3c369efec82fd9040a808a7c4bda394b89
JoungSik-MADX <joungsik@madx.company>
2020-07-08 14:48:49 +0900

    Updates

---
+++ precache-manifest.3e99dc5d163265bad3f81aad999631ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03e609534e0711a6af4460570e31361",
+    "url": "/OASM-web/index.html"
+  },
+  {
+    "revision": "7f796bfbd45fd57a26fa",
+    "url": "/OASM-web/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7d0daa79c41a58756013",
+    "url": "/OASM-web/static/js/2.cb52d844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OASM-web/static/js/2.cb52d844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f796bfbd45fd57a26fa",
+    "url": "/OASM-web/static/js/main.e5dc79c6.chunk.js"
+  },
+  {
+    "revision": "f913f4c1e0e674faed05",
+    "url": "/OASM-web/static/js/runtime-main.c07762c1.js"
+  }
+]);
