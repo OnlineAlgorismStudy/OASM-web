5cb6d90b31c174c0b9c9e7243dfd0599cc23900e
JoungSik <tjstlr2010@gmail.com>
2020-07-09 04:19:52 +0900

    [Add] Firebase Database 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/database";
 
 export default () => {
-  return <div>Hello World!</div>;
+  const database = firebase.database();
+
+  database.ref("users").on("value", (dataSnapshow) => {
+    console.log(dataSnapshow.val());
+  });
+
+  return <div>Hello World</div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYHjPAufAjSDR7b09_SfZxrFRN3IPIbX8",
+  authDomain: "oasm-889a7.firebaseapp.com",
+  databaseURL: "https://oasm-889a7.firebaseio.com",
+  projectId: "oasm-889a7",
+  storageBucket: "oasm-889a7.appspot.com",
+  messagingSenderId: "187405855435",
+  appId: "1:187405855435:web:318d1ae149f82e8743094e",
+  measurementId: "G-659EFPREVS",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
