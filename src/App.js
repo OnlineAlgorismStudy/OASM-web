4beebd8c8d7053da9f85ca235c5f39737bd56bc6
JoungSik-MADX <joungsik@madx.company>
2020-07-09 12:52:45 +0900

    [Add] Firebase 재등록

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as firebase from "firebase/app";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Add the Firebase services that you want to use
+import "firebase/database";
 
-export default App;
+export default () => {
+  const database = firebase.database();
+
+  database.ref("users").on("value", (dataSnapshow) => {
+    console.log(dataSnapshow.val());
+  });
+
+  return <div>Hello World</div>;
+};
