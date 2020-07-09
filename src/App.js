556920241a40a88337d99fa9e4e88a994179d08f
JoungSik <tjstlr2010@gmail.com>
2020-07-09 12:58:01 +0900

    [Add] 원상 복구

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 import * as firebase from "firebase/app";
 
 // Add the Firebase services that you want to use
 import "firebase/database";
 
 export default () => {
-  const database = firebase.database();
+  const [users, setUsers] = useState([]);
+  const [files, setFiles] = useState([]);
 
-  database.ref("users").on("value", (dataSnapshow) => {
-    console.log(dataSnapshow.val());
-  });
+  useEffect(() => {
+    const database = firebase.database();
+
+    database.ref("users").once("value", (data) => {
+      const result = Object.keys(data.val()).forEach((key, index) => {
+        setUsers(users.concat(data.val()[key]));
+      });
+    });
+
+    database.ref("files").on("value", (data) => {
+      Object.keys(data.val()).forEach((key) => {
+        if (key.match(moment().format("MMDD"))) {
+          console.log(data.val()[key]);
+        }
+      });
+    });
+  }, []);
 
   return <div>Hello World</div>;
 };
