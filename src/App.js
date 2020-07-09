04570e5b8a532177c083aacaa3f0ac740d98a1ed
JoungSik <tjstlr2010@gmail.com>
2020-07-09 14:51:58 +0900

    [Add] 오늘 기준, 업로드 했는지 안했는지 체크

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import _ from "lodash";
 import moment from "moment";
 import * as firebase from "firebase/app";
 
@@ -6,30 +8,67 @@ import * as firebase from "firebase/app";
 import "firebase/database";
 
 export default () => {
+  const database = firebase.database();
+
   const [users, setUsers] = useState([]);
-  const [files, setFiles] = useState([]);
+  const [complates, setComplates] = useState([]);
+  const [notComplates, setNotComplates] = useState([]);
 
   useEffect(() => {
-    const database = firebase.database();
+    database.ref("users").once("value", (data) => setUsers(data.toJSON()));
+  }, [database]);
 
-    database.ref("users").once("value", (data) => {
-      const result = Object.keys(data.val()).forEach((key, index) => {
-        setUsers(users.concat(data.val()[key]));
+  useEffect(() => {
+    if (!_.isEmpty(users)) {
+      database.ref("files").once("value", (data) => {
+        let result = [];
+        Object.keys(data.val()).forEach((key) => {
+          const file = data.val()[key];
+          if (file.date.match(moment().format("YYYY-MM-DD"))) {
+            result = result.concat(users[file.user].name);
+          }
+        });
+
+        let notResults = [];
+        Object.keys(users).forEach((key) => {
+          if (!result.includes(users[key].name)) {
+            notResults = notResults.concat(users[key].name);
+          }
+        });
+        setComplates(result);
+        setNotComplates(notResults);
       });
-    });
+    }
+  }, [database, users]);
 
-    database.ref("files").on("value", (data) => {
-      Object.keys(data.val()).forEach((key) => {
-        const file = data.val()[key];
-        if (file.date.match(moment().format("YYYY-MM-DD"))) {
-          console.log(file);
-        }
-        // if (file.match(moment().format("MMDD"))) {
-        //   console.log(file);
-        // }
-      });
-    });
-  }, []);
-
-  return <div>Hello World</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>
+          <Card style={{ textAlign: "center" }}>
+            <Card.Body>
+              <Card.Title>
+                {moment().format("YYYY-MM-DD")} 미달성 {notComplates.length}
+              </Card.Title>
+              {notComplates.map((notComplate) => (
+                <Card.Text>{notComplate}</Card.Text>
+              ))}
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+          <Card style={{ textAlign: "center" }}>
+            <Card.Body>
+              <Card.Title>
+                {moment().format("YYYY-MM-DD")} 달성 {complates.length}
+              </Card.Title>
+              {complates.map((complate) => (
+                <Card.Text>{complate}</Card.Text>
+              ))}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
