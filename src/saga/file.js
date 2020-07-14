cb5cc5bba03ece4cfc7ed8ab7623ef03cf359d15
JoungSik <tjstlr2010@gmail.com>
2020-07-15 02:51:17 +0900

    [#7][Fix] 키 수정

--- src/saga/file.js
+++ src/saga/file.js
@@ -1,15 +1,7 @@
 import { call, take, fork, all } from "redux-saga/effects";
-import {
-  FILE,
-  fileEntity,
-  ORIGIN,
-  originFileEntity,
-  // CREATE_FILE,
-  // createFileEntity,
-} from "redux/modules/file";
+import { FILE, fileEntity, ORIGIN, originFileEntity } from "redux/modules/file";
 import { fetchEntity } from "utils/saga";
 import firebase from "utils/firebase";
-// import { moment } from "moment";
 
 const getFiles = async (data) =>
   await firebase
@@ -39,32 +31,8 @@ const getOriginFiles = async () =>
       };
     });
 
-// const createFile = async (data) => {
-//   // 같은 키 값이라면 업데이트
-//   await data.map((file) => {
-//     firebase
-//       .database()
-//       .ref("files")
-//       .child(file.date)
-//       .child(file.key)
-//       .set(
-//         JSON.parse(
-//           JSON.stringify({
-//             date: file.date,
-//             name: file.name,
-//             extension: file.extension,
-//             message: file.message,
-//             upload: file.upload,
-//             user: file.key,
-//           })
-//         )
-//       );
-//   });
-// };
-
 const getFilesSaga = fetchEntity(fileEntity, getFiles);
 const getOriginFilesSaga = fetchEntity(originFileEntity, getOriginFiles);
-// const createFilesSaga = fetchEntity(createFileEntity, createFile);
 
 function* watchGetFiles() {
   while (true) {
@@ -80,17 +48,6 @@ function* watchGetOriginFiles() {
   }
 }
 
-// function* watchCreateFiles() {
-//   while (true) {
-//     const { payload } = yield take(CREATE_FILE);
-//     yield call(createFilesSaga, payload);
-//   }
-// }
-
 export default function* rootSaga() {
-  yield all([
-    fork(watchGetFiles),
-    fork(watchGetOriginFiles),
-    //    fork(watchCreateFiles),
-  ]);
+  yield all([fork(watchGetFiles), fork(watchGetOriginFiles)]);
 }
